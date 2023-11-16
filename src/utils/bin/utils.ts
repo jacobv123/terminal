06cf920e8a4b87a99dd3bfac155451740ed32fe8
Jacob Vandertuin <jacobvandertuin@me.com>
2023-11-16 21:13:54 +1100

    chore: customized utils.ts

--- src/utils/bin/utils.ts
+++ src/utils/bin/utils.ts
@@ -19,50 +19,12 @@ export const date = async (args: string[]): Promise<string> => {
   return new Date().toString();
 };
 
-export const gui = async (args: string[]): Promise<string> => {
-  window.open('https://m4tt72.com', '_self');
-
-  return 'Opening GUI version...';
-};
-
-export const email = async (args: string[]): Promise<string> => {
-  window.open('mailto:hi@m4tt72.com');
-
-  return 'Opening mailto:hi@m4tt72.com...';
-};
-
-export const vi = async (args: string[]): Promise<string> => {
-  return `why use vi? try 'emacs'.`;
-};
-
-export const vim = async (args: string[]): Promise<string> => {
-  return `why use vim? try 'emacs'.`;
-};
-
-export const emacs = async (args?: string[]): Promise<string> => {
-  return `really? emacs? you should be using 'vim'`;
-};
-
 export const sudo = async (args?: string[]): Promise<string> => {
   setTimeout(function () {
     window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
   }, 1000);
 
   return `Permission denied: unable to run the command '${args[0]}' as root.`;
-};
-
-export const repo = async (args?: string[]): Promise<string> => {
-  setTimeout(function () {
-    window.open('https://github.com/m4tt72/terminal', '_blank');
-  }, 1000);
-
-  return 'Opening repository...';
-};
-
-export const donate = async (args?: string[]): Promise<string> => {
-  window.open(packageJson.funding.url, '_blank');
-
-  return 'Opening donation url...';
 };
 
 export const banner = (args?: string[]): string => {
@@ -74,10 +36,10 @@ export const banner = (args?: string[]): string => {
 ╚██████╔╝███████╗██║   ██║  ██║╚██████╔╝██║ ╚████║
  ╚═════╝ ╚══════╝╚═╝   ╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═══╝ v${packageJson.version}
 
-Type 'help' to see list of available commands.
+Type 'help' to see a list of available commands.
 
 --
-The project is open-source 🎉 type 'repo' to check out the repository.
+ULTRON is online. Please state your intent.
 --
 `;
 };
